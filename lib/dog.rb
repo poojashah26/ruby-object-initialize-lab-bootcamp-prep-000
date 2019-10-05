68302f3cd3eda0c129141e70e994f7faea391c55
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-10-05 13:04:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 class Dog
   def initialize(dogs_name, dogs_breed = "Mutt")
-    @name = dog_name
+    @name = dogs_name
     @breed = dogs_breed
   end
   
-  def name=(dogs_name, dogs_breed = "Mutt"
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+  
+  def name 
 end
